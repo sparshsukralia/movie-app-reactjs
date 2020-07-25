0688595ca2a4612b532850ee6c0cf65041c42c75
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-07-26 02:29:55 +0545

    third

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "./components/Search";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
       <header className="App-header">
         <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search for a Movie..."
+        className="search"
+      />
+    </div>
+  );
+}
+
+export default Search;
