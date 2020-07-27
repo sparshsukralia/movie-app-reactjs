3513c26d7d83b22ca35232c02f7e5c5ed68ba2a2
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-07-28 02:48:08 +0545

    search-input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./components/Search";
 import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+
   const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=a2526df0";
+
+  const searchInput = (e) => {
+    let s = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+
+    console.log(state.s);
+  };
 
   return (
     <div className="App">
@@ -11,7 +27,7 @@ function App() {
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search />
+        <Search searchInput={searchInput} />
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Search.css";
 
-function Search() {
+function Search({ searchInput }) {
   return (
     <div className="search-bar">
       <input
         type="text"
         placeholder="Search for a Movie..."
         className="search"
+        onChange={searchInput}
       />
     </div>
   );
