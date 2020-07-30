71d88225f69b358a189e8474ed447a52a24d8ee0
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-07-31 01:34:20 +0545

    Updates

---
+++ precache-manifest.488cf92891c3aca0e1dc5a975bad9da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d3804d444b4576b18464cccbce02e5",
+    "url": "/movie-app-reactjs/index.html"
+  },
+  {
+    "revision": "7b56e04e4dcf7826a76b",
+    "url": "/movie-app-reactjs/static/css/main.6b372cf0.chunk.css"
+  },
+  {
+    "revision": "13d62e2cdffc03bac871",
+    "url": "/movie-app-reactjs/static/js/2.d2e1a7e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-reactjs/static/js/2.d2e1a7e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b56e04e4dcf7826a76b",
+    "url": "/movie-app-reactjs/static/js/main.2979fc4c.chunk.js"
+  },
+  {
+    "revision": "c81d11b5bc1ea9b1ed86",
+    "url": "/movie-app-reactjs/static/js/runtime-main.1680e6c7.js"
+  }
+]);
